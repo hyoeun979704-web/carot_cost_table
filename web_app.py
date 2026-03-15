35def88f4b05fbd1f9e7d195748a687ece9f58d5
Hyoeun Kim <hyoeun979704@gmail.com>
2026-03-15 19:01:02 +0900

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -163,4 +163,4 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("web_app:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("web_app:app", host="0.0.0.0", port=8000, reload=True)
